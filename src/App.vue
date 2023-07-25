f83f859b7757ca0e9c417fda7ee453d45dacc36d
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-07-26 05:58:40 +0800

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,13 @@
 <script setup>
-import TodoCard from './components/TodoCard.vue';
-
-
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <main>
-    <TodoCard/>
-  </main>
+  
+
+  <RouterView />
 </template>
 
 <style>
-  * {
-    list-style: none;
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    color: black;
-    font-family: Montserrat;
-  }
 
-  body {
-    background: white;
-    margin: 0;
-    padding: 0;
-    background: #8E2DE2;                         
-    background: -webkit-linear-gradient(to right, #4A00E0, #8E2DE2);
-    background: linear-gradient(to right, #4A00E0, #8E2DE2);
-
-  }
-</style>
-
-<style scoped>
-  main {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  }
 </style>
