bdc21411faa8226f31078058c528bfb74892f701
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-07-27 07:53:16 +0800

    Updated

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+
 </script>
 
-<template>
-  
 
+<template>
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,40 @@
 <script setup>
+  import { ref } from 'vue';
+
+  const count = ref(0);
+
+  function timesItself() {
+    return count.value * count.value;
+  }
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <h1>The current count is</h1>
+    <p>{{count}}</p>
+    <div>
+      <button @click="timesItself()">-</button>
+      <button @click="count++" >+</button>
+    </div>
+  </div>
 </template>
+
+
+
+<style scoped>
+  *{
+    color: black;
+  }
+
+  .wrapper {
+    width: 100vw;
+    height: 100vh;
+    display: flex;  
+    justify-content: center;
+    align-items: center;
+    background: aliceblue;
+    flex-direction: column;
+  }
+
+</style>
